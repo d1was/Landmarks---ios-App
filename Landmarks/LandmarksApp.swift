a6a31affb27a916c0052681edc66e0eb16571424
Diwas KC <diwas@rvlife.com>
2024-08-03 21:30:56 +0545

    Initial Commit

---
+++ Landmarks/LandmarksApp.swift
@@ -0,0 +1,17 @@
+//
+//  LandmarksApp.swift
+//  Landmarks
+//
+//  Created by Diwas KC on 03/08/2024.
+//
+
+import SwiftUI
+
+@main
+struct LandmarksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
